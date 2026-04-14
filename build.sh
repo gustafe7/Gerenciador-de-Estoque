37307f59691ce86761420496183026cff9191b6d
Gustavo Gomes <gustavocjr.gg@gmail.com>
2026-04-14 14:33:28 -0300

    adicionar collectstatic para arquivos estáticos em produção

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 pip install -r requirements.txt
 python manage.py migrate
+python manage.py collectstatic --noinput
 echo "from django.contrib.auth.models import User; User.objects.filter(username='${DJANGO_SUPERUSER_USERNAME}').exists() or User.objects.create_superuser('${DJANGO_SUPERUSER_USERNAME}', '', '${DJANGO_SUPERUSER_PASSWORD}')" | python manage.py shell
